baf84b60e797d8399724f79295de7f422bd25794
Kapil Chaudhary <108571992+Kapil685838@users.noreply.github.com>
2023-03-03 22:06:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-let yourArray = ['str', 1, true, undefined, null];
+let yourArray = [];
+for(let i = 0; i < 5; i++){
+	let obj = {`str&{i + 1}`, i, true};
+	yourArray.push(obj);
+}
 console.log(yourArray.length);
